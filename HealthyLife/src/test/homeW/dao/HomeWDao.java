dd9d80e1bf1df7e0da09f7482278be95db421fc7
JI-3617 <ziaiet37@gmail.com>
2021-07-03 13:04:31 +0900

    새글 저장, 글 하나 정보 리턴, 글 목록 리턴하는 메소드 추가

--- HealthyLife/src/test/homeW/dao/HomeWDao.java
+++ HealthyLife/src/test/homeW/dao/HomeWDao.java
@@ -1,5 +1,14 @@
 package test.homeW.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.homeW.dto.HomeWDto;
+import test.util.DbcpBean;
+
 public class HomeWDao {
 	
 	private static HomeWDao dao;
@@ -11,6 +20,195 @@ public class HomeWDao {
 		return dao;
 	}
 	
+
 	//글 목록을 리턴하는 메소드
+	public List<HomeWDto> getList(HomeWDto dto){
+		//글 목록을 담을 ArrayList 객체 생성
+		List<HomeWDto> list=new ArrayList<HomeWDto>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 작성
+			String sql ="SELECT *"
+					+"	FROM"
+					+" 		(SELECT result1.* ROWNUM AS rnum"
+					+" 		FROM"
+					+" 			(SELECT num, writer, title, viewCount, regdate"
+					+" 			FROM home_workout"
+					+" 			ORDER BY num DESC) result1)"
+					+"			WHERE rnum BETWEEN ? AND ?";
+					
+			//PreparedStatement 객체의 참조값 얻어오기
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩할 내용이 있으면 여기서 바인딩
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
+			//select 문 수행하고 결과를 ResultSet 으로 받아오기
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 ResultSet 객체에 있는 내용을 추출해서 원하는 Data type 으로 포장하기
+			while (rs.next()) {
+				HomeWDto dto2=new HomeWDto();
+				dto2.setNum(rs.getInt("num"));
+				dto2.setWriter(rs.getString("writer"));
+				dto2.setTitle(rs.getString("title"));
+				dto2.setViewCount(rs.getInt("viewCount"));
+				dto2.setRegdate(rs.getString("regdate"));
+				list.add(dto2);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
 	
+	//글 하나의 정보를 리턴하는 메소드
+	public HomeWDto getData(int num) {
+		HomeWDto dto=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 작성
+			String sql = " SELECT num, title, writer, content, viewCount, regdate"
+					+" FROM home_workout"
+					+" WHERE num=?";
+			//PreparedStatement 객체의 참조값 얻어오기
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩할 내용이 있으면 여기서 바인딩
+			pstmt.setInt(1, num);
+			//select 문 수행하고 결과를 ResultSet 으로 받아오기
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 ResultSet 객체에 있는 내용을 추출해서 원하는 Data type 으로 포장하기
+			if (rs.next()) {
+				dto=new HomeWDto();
+				dto.setNum(rs.getInt("num"));
+				dto.setTitle(rs.getString("title"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setContent(rs.getString("content"));
+				dto.setViewCount(rs.getInt("regdate"));
+				dto.setRegdate(rs.getString("regdate"));
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return dto;
+	}
+	
+	//글 하나의 정보를 리턴하는 메소드 
+	public HomeWDto getData(HomeWDto dto) {
+		HomeWDto dto2=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 작성
+			String sql ="SELECT *"
+					+" FROM"
+					+" 		(SELECT num, writer, title, content, viewCount, regdate,"
+					+" 		LAG(num, 1, 0) OVER(ORDER BY num DESC) AS prevNum,"
+					+" 		LEAD(num, 1, 0) OVER(ORDER BY num DESC) AS nextNum"
+					+" 		FROM home_workout"
+					+" 		ORDER BY num DESC)"
+					+" WHERE num=?";
+					
+			//PreparedStatement 객체의 참조값 얻어오기
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩할 내용이 있으면 여기서 바인딩
+			pstmt.setInt(1, dto.getNum());
+			//select 문 수행하고 결과를 ResultSet 으로 받아오기
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 ResultSet 객체에 있는 내용을 추출해서 원하는 Data type 으로 포장하기
+			if (rs.next()) {
+				dto2=new HomeWDto();
+				dto2.setNum(rs.getInt("num"));
+				dto2.setTitle(rs.getString("title"));
+				dto2.setWriter(rs.getString("writer"));
+				dto2.setContent(rs.getString("content"));
+				dto2.setViewCount(rs.getInt("viewCount"));
+				dto2.setRegdate(rs.getString("regdate"));
+				dto2.setPrevNum(rs.getInt("prevNum"));
+				dto2.setNextNum(rs.getInt("nextNum"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return dto2;
+	}
+	
+	//새글 저장하는 메소드
+	public boolean insert(HomeWDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 작성
+			String sql = "INSERT INTO home_workout"
+					+" (num, writer, title, content, viewCount, regdate)"
+					+" VALUES(home_workout_seq.NEXTVAL, ?, ?, ?, ?, SYSDATE)";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩할 내용이 있으면 여기서 바인딩
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getTitle());
+			pstmt.setString(3, dto.getContent());
+			pstmt.setInt(4, dto.getViewCount());
+		
+			//insert or update or delete 문 수행하고 변화된 row 의 갯수 리턴 받기
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 }
