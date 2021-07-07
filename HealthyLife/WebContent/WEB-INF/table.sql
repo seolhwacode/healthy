fd66bf37c86065626dd9a54ca1b936c9897f1321
DESKTOP-OGKSF7E\acorn <seolhwacode@gmail.com>
2021-07-07 11:07:16 +0900

    [add] video_board_comment, video_board_comment_seq 추가

--- HealthyLife/WebContent/WEB-INF/table.sql
+++ HealthyLife/WebContent/WEB-INF/table.sql
@@ -34,3 +34,20 @@ FROM
 	ORDER BY num DESC)
 WHERE num = 21;
 
+-- 비디오 자료실 댓글 table
+CREATE TABLE video_board_comment(
+   num NUMBER PRIMARY KEY, --댓글의 글번호
+   writer VARCHAR2(100), --댓글 작성자의 아이디
+   content VARCHAR2(500), --댓글 내용
+   target_id VARCHAR2(100), --댓글의 대상자 아이디
+   ref_group NUMBER,	-- 댓글을 단 게시글 번호(num)
+   --  / 원글에 댓글을 달면 댓글 num = comment_group 같다 -> 들여쓰기 X / 댓글에 댓글을 쓴 경우 num != comment_group -> 들여쓰기
+   comment_group NUMBER,	-- 게시글에 작성한 댓글 번호
+   deleted CHAR(3) DEFAULT 'no',	-- no : 삭제 X, yes : 삭제 => 삭제된 댓글입니다. 출력
+   regdate DATE
+);
+
+-- 비디오 자료실 댓글의 seq
+CREATE SEQUENCE video_board_comment_seq;
+
+
